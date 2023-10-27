ae621918d3ee95baf01ba614142a6ad6e0666f5e
rajuanalam <rajuanalam2611@gmail.com>
2023-10-28 00:56:16 +0600

    first commit

---
+++ Js/tailwind.config.js
@@ -0,0 +1,53 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        // custom code start
+        // color start
+        colors: {
+          headercolor: "#1C1E53",
+          navtxtcolor:"#BBBBCB",
+          contactbtncolor: "#FCD980",
+          buttonborder: "rgba(244, 246, 252, 0.20)",
+          buttontxthover:"#1B1C2B",
+          workbgcolor:"#F4F6FC",
+          featurebgcolor:"#F4F6FC",
+          aboutusbgcolor:"rgba(244, 246, 252, 0.50)",
+          arrowbuttoncolor:"#5239FA",
+          arrowcolor:"#282938",
+          bluebuttoncolor:"#2405F2",
+          frombordercolor:"rgba(255, 255, 255, 0.05)",
+          headingcolor: "#282938",
+          gettouchcolor:"#2405F2",
+          cardhovercolor:"#1C1E53",
+          footerbgtwo:"#FCD980",
+        },
+
+        // color end
+        // All Containers Start
+        maxWidth: {
+            headercontainer: "1280px",
+ 
+        },
+        // All Containers End
+        // Background Images
+        backgroundImage: {
+            inquiryBg: "url('images/inquirybg.png')",
+        },
+
+        // Background Images
+        // Padding
+      
+        // Padding
+        // Font Family
+        fontFamily: {
+            Manrope: ['Manrope',"sans-serif"],
+            Poppins: ['Poppins','sans-serif'],
+          
+
+
+          },
+        // Font Family
+        // custom code end
+      }
+    }
+  }
